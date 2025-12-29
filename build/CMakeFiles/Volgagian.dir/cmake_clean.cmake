c5be1d068169538273adb0452ee9901fef6127f1
staruwos <juliotrasferetti@gmail.com>
2025-12-29 11:36:12 -0300

    fix

---
+++ build/CMakeFiles/Volgagian.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Volgagian.dir/link.d"
+  "CMakeFiles/Volgagian.dir/src/ModelRenderer.cpp.o"
+  "CMakeFiles/Volgagian.dir/src/ModelRenderer.cpp.o.d"
+  "CMakeFiles/Volgagian.dir/src/main.cpp.o"
+  "CMakeFiles/Volgagian.dir/src/main.cpp.o.d"
+  "bin/Volgagian"
+  "bin/Volgagian.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Volgagian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
